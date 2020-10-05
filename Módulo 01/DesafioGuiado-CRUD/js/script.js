fb75ca5a3703ff0b8ee87de5e4f2814fe109c650
carolfons <carolinefonseca@gec.inatel.br>
2020-10-05 14:08:55 -0300

    primeiro módulo

--- Módulo 01/DesafioGuiado-CRUD/js/script.js
+++ Módulo 01/DesafioGuiado-CRUD/js/script.js
@@ -1,14 +1,32 @@
 window.addEventListener('load', start);
 
 var globalNames = ['um', 'dois', 'três', 'quatro'];
+var inputName = null;
 
 function start() {
+  inputName = document.querySelector('#inputName');
   preventFormSubmit();
+  activateInput();
 }
 
 function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
   var form = document.querySelector('form');
   form.addEventListener('submit', handleFormSubmit);
 }
 
-function handleFormSubmit() {}
+function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
+  }
+  function handleTyping(event) {
+    if (event.key === 'Enter') {
+      // console.log(event.target.value);-> pega o que foi escrito antes de apertar o 'enter'
+      insertName(event.target.value);
+    }
+  }
+  inputName.addEventListener('keyup', handleTyping);
+  inputName.focus();
+}
